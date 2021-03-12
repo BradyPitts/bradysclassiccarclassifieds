84bf792c64972f10d81f8428d9268956ab4f85a2
Brady Pitts <this.is.bp67c@gmail.com>
2021-03-11 19:29:16 -0700

    finished for the night

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Header />,
+    <Forsale />,
+    <MyGarage />
   );
 }
 
